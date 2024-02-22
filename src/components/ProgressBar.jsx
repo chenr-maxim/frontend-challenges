2cca1a9b173fbfd49711e837fd1d85f03287ecf3
Maxim Chen <chenr.maxim@gmail.com>
2024-02-21 22:35:49 -0800

    feat: v2 added breakpoints toggle, and update progresbar slow-animation

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { PROGRESS_BAR_BREAKPOINTS } from '../labels/constants';
 
 import './ProgressBar.scss';
 
-const ProgressBar = ({ fade, progress }) => {
+const ProgressBar = ({ fade, progress, toggleBreakpoint }) => {
+  const [isApproaching, setIsApproaching] = useState(false);
+  const [progressWidth, setProgressWidth] = useState(0);
+  const progressBarRef = useRef(null);
+  const progressBarWrapperRef = useRef(null);
+
+  // we check if progress is within range of a breakpoint and if it is
+  // update boolean so we can dynamically add slow-animation css class
+  useEffect(() => {
+    if (toggleBreakpoint) {
+      if (progress) {
+        if (
+          PROGRESS_BAR_BREAKPOINTS.some(
+            (elem) => elem - 5 <= progress && progress <= elem + 5
+          )
+        ) {
+          setIsApproaching(true);
+        } else {
+          setIsApproaching(false);
+        }
+      }
+      // const percentageOfWrapper = Math.floor(
+      //   (progressBarRef.current.clientWidth /
+      //     progressBarWrapperRef.current.clientWidth) *
+      //     100
+      // );
+      // setProgressWidth(percentageOfWrapper);
+    }
+  }, [progress]);
+
   return (
     <div className={`progress-bar-row ${fade ? 'hide' : 'show'}`}>
-      <div className="progress-bar-wrapper">
+      <div ref={progressBarWrapperRef} className="progress-bar-wrapper">
         <div
-          className="progress-bar"
+          ref={progressBarRef}
+          className={`progress-bar ${isApproaching ? 'slow-animation' : ''}`}
           style={{
             width: `${progress}%`,
           }}></div>
--- src/labels/constants.js
+++ src/labels/constants.js
@@ -7,3 +7,4 @@ export const REQUEST_DURATION = 15;
 export const REQUEST_DURATION_90_PERC = 90;
 export const HALF_SECOND = 500;
 export const ONE_SECOND = 1000;
+export const PROGRESS_BAR_BREAKPOINTS = [10, 25, 30, 65, 85];
